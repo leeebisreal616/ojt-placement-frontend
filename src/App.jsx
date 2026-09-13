f45d2821d502517982d917ff4dad667520cc405e
Aaron Valencia <leeebisreal616@gmail.com>
2026-09-14 00:27:29 +0800

    Add protected route wrapper for authenticated pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import CreatePlacement from './pages/CreatePlacement';
 import PlacementDetail from './pages/PlacementDetail';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/placements/new" element={<CreatePlacement />} />
-      <Route path="/placements/:id" element={<PlacementDetail />} />
+      <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+      <Route path="/placements/new" element={<ProtectedRoute><CreatePlacement /></ProtectedRoute>} />
+      <Route path="/placements/:id" element={<ProtectedRoute><PlacementDetail /></ProtectedRoute>} />
     </Routes>
   );
 }
